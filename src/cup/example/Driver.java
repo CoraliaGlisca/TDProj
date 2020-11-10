f6ad2422e418448eb2d0fbf868c394512fb9f80a
CoraliaGlisca <32877341+CoraliaGlisca@users.noreply.github.com>
2020-11-10 19:25:58 +0200

    h4

--- src/cup/example/Driver.java
+++ src/cup/example/Driver.java
@@ -1,6 +1,8 @@
 package cup.example;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
@@ -10,12 +12,27 @@ import java_cup.runtime.*;
 class Driver {
 
 	  public static void main(String[] args) throws Exception {
-	        InputStream s = new FileInputStream("input.txt");
-	        Reader r = new InputStreamReader(s);
-	        Lexer l = new Lexer(r);
-	        Symbol currentSymbol = null;
-	        while((currentSymbol=l.next_token()).sym != 0) {
-	            System.out.println("currentSymbol = "+ l.yytext());
+//	        InputStream s = new FileInputStream("input.txt");
+//	        Reader r = new InputStreamReader(s);
+//	        Lexer l = new Lexer(r);
+//	        Symbol currentSymbol = null;
+//	        
+//	        while((currentSymbol=l.next_token()).sym != 0) {
+//	            System.out.println("currentSymbol = "+ l.yytext());
+//	        }
+	        //Parser parser = new Parser();
+	        //parser.parse();
+		  File file = new File("input.txt");
+		  FileInputStream fis = null;
+		  try {
+			  fis = new FileInputStream(file);
+		  } catch (IOException e) {
+			  e.printStackTrace();
+		  }
+		  Lexer lexer = new Lexer(fis);
+		  Symbol s = lexer.next_token();
+		  while((s = lexer.next_token()).sym != 0) {
+	            System.out.println("currentSymbol = "+ lexer.yytext());
 	        }
 	    }
 	
